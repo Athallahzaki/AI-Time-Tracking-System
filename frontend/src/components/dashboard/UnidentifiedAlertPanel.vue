e846807bbb7bdbfd2720e1a4cb4f204918d410ef
kevin afton <lolbited@gmail.com>
2026-09-24 00:58:29 +0700

    Fixing....

--- frontend/src/components/dashboard/UnidentifiedAlertPanel.vue
+++ frontend/src/components/dashboard/UnidentifiedAlertPanel.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { useUnidentifiedAlerts } from '@/composables/usenIdentifiedAlerts';
+import { useUnidentifiedAlerts } from '@/composables/useUnidentifiedAlerts';
 import { AlertTriangle, Video } from '@lucide/vue';
 
 const { alerts, isLoading, loadError } = useUnidentifiedAlerts();
